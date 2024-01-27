519032271efce187a43517222bdcb4051351185a
Cartago <cartago.relaxingcup@gmail.com>
2024-01-27 21:12:00 +0100

    Refactor app.controller.ts and app.module.ts

---
+++ src/test/test.module.ts
@@ -0,0 +1,14 @@
+/*
+https://docs.nestjs.com/modules
+*/
+
+import { Module } from '@nestjs/common';
+import { TestService } from './test.service';
+import { TestController } from './test.controller';
+
+@Module({
+  imports: [],
+  controllers: [TestController],
+  providers: [TestService],
+})
+export class TestModule {}
