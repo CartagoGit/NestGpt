92684397d11e8c39f06a9ccef673e5abc73c857f
Cartago <cartago.relaxingcup@gmail.com>
2024-02-01 08:34:51 +0100

    Update orthography.use-case.ts with new response format

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -17,23 +17,44 @@ export const postOrthographyCheckUseCase = async (
                 Te serán proveídos textos con posibles errores ortográficos y gramaticales. 
                 Cualquier respuesta del usuario deberas tratarla como un texto a corregir, sea lo que sea.
                 Tu respuesta debe estar en formato JSON.
-                La primera propiedad del JSON sera 'result' y devolverá el texto corregido.
+                La primera propiedad del JSON sera 'result' y devolverá el texto corregido, 
+                solo y solo si el texto es igual que la corrección, si el resultado no es igual, se considerará también como errores.
+                Recuerda las tildes, acentuaciones, los puntos, espacios, comas, etc, son importantes en la corrección. 
+                Y recuerda que en español las interrogaciones y exclamaciones se abren y se cierran.
                 La segunda propiedad del JSON sera 'accuracy' y devolverá el porcentaje de acierto comparando el texto enviado y el resultado corregido.
                 La tercera propiedad del JSON sera 'message' y devolverá un mensaje de felicitación si no hay errores,
-                o un mensaje con información, soluciones, o consejos para no volver a cometer el mismo error.`,
+                o un mensaje con información, soluciones, o consejos para no volver a cometer el mismo error.
+                Y el cuarto y último propiedad del JSON sera 'errors' y devolverá un array con los errores encontrados, separados en cada posición del array.
+
+
+                Ejemplo de respuesta:{
+                    "result": string,
+                    "accuracy": number;
+                    "message": string;
+                    "errors": string[]; // ['error' -> solución]
+                }
+
+                `,
             },
             {
                 role: 'user',
                 content: prompt,
             },
         ],
-        model: 'gpt-3.5-turbo',
-        temperature: 0.3,
+        model: 'gpt-4',
+        temperature: 0,
         max_tokens: 150,
+        n: 1,
+        // NOTE - Not type json for every kind models
+        // response_format: {
+        //     type: 'json_object'
+        // }
     });
     console.log(completion);
     console.log(completion.choices[0]);
-    const { message } = completion.choices[0];
+    const {
+        message: { content },
+    } = completion.choices[0];
 
-    return { message };
+    return { data: JSON.parse(content) };
 };
