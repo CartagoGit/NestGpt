e45f048498ec023b79af3ef011e15eb083126440
Cartago <cartago.relaxingcup@gmail.com>
2024-02-10 16:35:52 +0100

    Actualizar mensajes de respuesta en formato markdown y agregar títulos a las preguntas de usuario en los casos de uso pro-con-discusser y pro-con-stream.

--- src/gpt/use-cases/pro-con-stream.use-case.ts
+++ src/gpt/use-cases/pro-con-stream.use-case.ts
@@ -12,7 +12,7 @@ export const postProConStreamUseCase = async (
             {
                 role: 'system',
                 content: `Se te dará una pregunta y tu tarea es dar una respuesta con pros y contras,
-                la respuesta debe de ser en formato markdown,
+                la respuesta debe de ser en formato markdown y debe tener un titulo que sera la pregunta del usuario.
                 los pros y contras deben de estar en una lista.
                 Ten en cuenta que tienes ${maxTokens - 100} como máximo tokens para realizar tu respuesta, incluyendo este prompt, así que no dejes una respuesta a medias. Termina la respuesta antes de que se acaben los tokens. Quiero una respuesta escueta, no me des una respuesta larga.`,
             },
