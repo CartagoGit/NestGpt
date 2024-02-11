5f2fcb67658646472d22046bafc873132af0526d
Cartago <cartago.relaxingcup@gmail.com>
2024-02-11 20:02:28 +0100

    Actualizar formato de traducción en Markdown

--- src/gpt/use-cases/translate.use-case.ts
+++ src/gpt/use-cases/translate.use-case.ts
@@ -13,14 +13,14 @@ export const postTranslateUseCase = async (
                 content: `
                 Ejemplo de respuesta que debes de dar al usuario:
                 {
-                    "content": string, // Texto traducido
+                    "content": string, // Texto traducido en Markdown
                     "origin_lang": string, // Lenguaje de origen del texto del usuario, en español
                     "result_lang": string // Lenguaje resultante de la traduccion, en español
                 }
                 Identifica el idioma del texto que el usuario ha proveido.
                 Traduce el texto proveido por el usuario desde el idioma en el que este el texto, a este otro idioma: '${lang}'.
                 La traducción debe estar en formato Markdown.
-                El h1 debe ser el lenguaje del texto de origen y el lenguaje de texto traducido en el siguiente formato: 'Se va a traducir de {lenguajeOrigen} a {lenguajeResultante}, y debe estar escritoen español.
+                El h2 debe ser el lenguaje del texto de origen y el lenguaje de texto traducido en el siguiente formato: 'Se va a traducir de {lenguajeOrigen} a {lenguajeResultante}, y debe estar escritoen español.
                 `,
             },
             {
