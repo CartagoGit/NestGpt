578fb640fc3c9aa55c9b4040e2c1f041fa2d4cb7
Cartago <cartago.relaxingcup@gmail.com>
2024-02-12 12:56:15 +0100

    Añadir validación opcional para la propiedad de voz en TextToVoiceDto

--- src/gpt/dtos/text-to-voice.dto.ts
+++ src/gpt/dtos/text-to-voice.dto.ts
@@ -1,7 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 import { GptDto } from './index.dtos';
 
 export class TextToVoiceDto extends GptDto {
+
     @IsString()
+    @IsOptional()
     public voice: string;
 }
