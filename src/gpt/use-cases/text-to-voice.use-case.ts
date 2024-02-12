116e5fa283f2d8c6fe449298620780476a04874c
Cartago <cartago.relaxingcup@gmail.com>
2024-02-12 14:03:18 +0100

    Actualización de dependencias y código de generación de voz

--- src/gpt/use-cases/text-to-voice.use-case.ts
+++ src/gpt/use-cases/text-to-voice.use-case.ts
@@ -1,12 +1,24 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import shortUuid from 'short-uuid';
 import OpenAI from 'openai';
 import { TextToVoiceDto } from '../dtos/text-to-voice.dto';
-import { IApiAudio } from 'src/shared/interfaces/api.interface';
+import { IApiAudio } from 'src/shared/interfaces/index.interfaces';
+
+const uuid = shortUuid();
 
 export const postTextToVoiceUseCase = async (
     openAi: OpenAI,
     dto: TextToVoiceDto,
 ): IApiAudio => {
     const { voice, prompt, format, model } = dto;
+
+    const folderPath = path.resolve(__dirname, '../../../generated/audios');
+    const filePath = path.resolve(
+        folderPath,
+        `${new Date().getTime().toString().padStart(14, '0')}_${uuid.new()}.${format}`,
+    );
+
     const completion = await openAi.audio.speech.create({
         input: prompt,
         model,
